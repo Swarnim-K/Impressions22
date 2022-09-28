74751368b63659bcefc150cbf26fa8963b397849
Swarnim-K <kambless.elec@coep.ac.in>
2022-09-29 01:54:16 +0530

    about us mediaques

---
+++ scripts/timeline.js
@@ -0,0 +1,21 @@
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from(".aboutUsTitle", {
+    duration: 2,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".aboutUsTitle",
+        toggleActions: "restart none none none"
+    }
+})
+
+gsap.from(".moduletext", {
+    duration: 2,
+    opacity: 0,
+    y: "3vh",
+    stagger: 0.25,
+    scrollTrigger: {
+        trigger: ".moduletext",
+        toggleActions: "restart none none none"
+    }
+})
