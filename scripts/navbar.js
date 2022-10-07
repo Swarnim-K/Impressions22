9d9aa5d7b3fac118def60d120c32ef7a3462c81a
Swarnim-K <kambless.elec@coep.ac.in>
2022-10-07 20:06:32 +0530

    hamburger menu

---
+++ scripts/navbar.js
@@ -0,0 +1,16 @@
+const navbar = document.querySelector(".navbar")
+const sitelogo = document.querySelector(".siteLogo")
+const countdownCtn = document.querySelector(".countdown-container")
+
+window.addEventListener("scroll", () => {
+    if (innerWidth >= 768) {
+        if (window.scrollY > countdownCtn.offsetTop) {
+            sitelogo.classList.add("glassmorphic")
+            navbar.classList.add("glassmorphic")
+        }
+        if (window.scrollY < countdownCtn.offsetTop) {
+            sitelogo.classList.remove("glassmorphic")
+            navbar.classList.remove("glassmorphic")
+        }
+    }
+})
