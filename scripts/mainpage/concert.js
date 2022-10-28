fad78b46bd052bb59318f3e10a752ed595c59929
Swarnim-K <kambless.elec@coep.ac.in>
2022-10-28 12:42:29 +0530

    concert idea

---
+++ scripts/mainpage/concert.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    var elem = $('.concert-container'),
+        pos = elem.offset(),
+        elem_left = pos.left,
+        elem_top = pos.top,
+        elem_width = elem.width(),
+        elem_height = elem.height(),
+        x_center,
+        y_center;
+
+
+    $('.concert-container').mousemove(function (e) {
+
+        x_center = (elem_width / 2) - (e.pageX - elem_left);
+        y_center = (elem_height / 2) - (e.pageY - elem_top);
+
+        $('.parallax').each(function () {
+
+            var speed = $(this).attr('data-speed'),
+                xPos = Math.round(-1 * x_center / 20 * speed),
+                yPos = Math.round(y_center / 20 * speed);
+
+            if (yPos < 0)
+                yPos = -2 * speed;
+
+            $(this).css('transform', 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0px)');
+
+        });
+
+    });
+
+});
