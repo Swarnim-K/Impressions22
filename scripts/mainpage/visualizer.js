842017c9d8c1d8bfcd07321706d96f37abfbb2da
Swarnim-K <kambless.elec@coep.ac.in>
2022-11-22 18:44:41 +0530

    visualizer final

--- scripts/mainpage/visualizer.js
+++ scripts/mainpage/visualizer.js
@@ -68,7 +68,6 @@ var media = [
     avg, ctx, actx, asource, gainNode, analyser, frequencyData, frequencyDataLength, timeData;
 
 
-playButton.classList.add("preloader-remove")
 window.addEventListener('load', () => {
     initialize();
 });
@@ -119,7 +118,6 @@ function initializeAudio() {
         analyser.smoothingTimeConstant = 0.8;
 
         actx.decodeAudioData(this.response, function (buffer) {
-            console.timeEnd('decoding audio data');
 
             msgElement.textContent = "- Enter the Pravah -";
             playButton.classList.remove("preloader-remove")
@@ -146,6 +144,7 @@ function initializeAudio() {
 function createAudioControls() {
     var preloader = document.querySelector(".preloader");
     var pauseButton = document.querySelector(".siteLogo");
+    var headphone = document.querySelector(".headphone");
 
 
     playButton.addEventListener('click', function (e) {
@@ -155,6 +154,7 @@ function createAudioControls() {
         }, 10000)
         preloader.classList.add("preloader-remove")
         playButton.classList.add("preloader-remove")
+        headphone.classList.add("preloader-remove")
         return playAudio();
     });
     pauseButton.addEventListener('click', function (e) {
@@ -495,3 +495,41 @@ function resizeHandler() {
         p.updateDynamics();
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const open = document.querySelector('.container');
+const close = document.querySelector('.close');
+var tl = gsap.timeline({ defaults: { duration: 1, ease: 'expo.inOut' } });
+open.addEventListener('click', () => {
+    if (tl.reversed()) {
+        tl.play();
+    } else {
+        tl
+            .set($('canvas'), { css: { zIndex: 450 } })
+            .to('nav', { right: 0 })
+            .to('nav', { height: '100vh' }, '-=.1')
+            .to('nav ul li a', { opacity: 1, pointerEvents: 'all', stagger: .2 }, '-=.8')
+            .to('.close', { opacity: 1, pointerEvents: 'all' }, "-=.8")
+            .to('nav h2', { opacity: 1 }, '-=1');
+    }
+});
+
+close.addEventListener('click', () => {
+    tl.reverse();
+});
