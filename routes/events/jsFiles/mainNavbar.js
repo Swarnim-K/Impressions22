21a95a3a898fadff695b142a308f701e3c96035b
Abhishek <shindeaa21.comp@coep.ac.in>
2022-11-29 20:16:58 +0530

    navbar changes in events page

--- routes/events/jsFiles/mainNavbar.js
+++ routes/events/jsFiles/mainNavbar.js
@@ -1,13 +1,23 @@
-var mnbar = document.getElementById("mainNavbar");
-var barsIcon = document.getElementById("barsIcon");
+const open = document.querySelector('.container2');
+const close = document.querySelector('.close');
+var tl2 = gsap.timeline({ defaults: { duration: 1, ease: 'expo.inOut' } });
+open.addEventListener('click', () => {
+    if (tl2.reversed()) {
+        tl2.play();
+    } else {
+        tl2
+            .set($('canvas'), { css: { zIndex: 450 } })
+            .set($('.navbar2'), { css: { zIndex: 400 } })
+            .set($('.topLine'), { css: { zIndex: 400 } })
+            .set($('.twoLogos'), { css: { zIndex: 400 } })
+            .to('.nav-content', { right: 0 })
+            .to('.nav-content', { height: '100vh' }, '-=.1')
+            .to('.nav-content ul li a', { opacity: 1, pointerEvents: 'all', stagger: .1 }, '-=.5')
+            .to('.close', { opacity: 1, pointerEvents: 'all' }, "-=.8")
+            .to('.nav-content h2', { opacity: 1 }, '-=1');
+    }
+});
 
-function showHidemnbar() {
-    if (mnbar.style.display == "flex") {
-        mnbar.style.display = "none";
-        barsIcon.src = "images/icons8-menu-100.png";
-    }
-    else {
-        mnbar.style.display = "flex";
-        barsIcon.src = "images/icons8-close-96.png";
-    }
-}
+close.addEventListener('click', () => {
+    tl2.reverse();
+});
